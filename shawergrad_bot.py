9568f03d871ba2e7005d18622deacc7c5d732ff5
antONget <a.l.ponomare@mail.ru>
2024-03-23 11:19:18 +0300

    add пишем логи в файл

--- shawergrad_bot.py
+++ shawergrad_bot.py
@@ -18,6 +18,8 @@ async def main():
     # Конфигурируем логирование
     logging.basicConfig(
         level=logging.INFO,
+        filename="py_log.log",
+        filemode='w',
         format='%(filename)s:%(lineno)d #%(levelname)-8s '
                '[%(asctime)s] - %(name)s - %(message)s')
     # logging.basicConfig(level=logging.INFO, filename="py_log.log", filemode="w")
